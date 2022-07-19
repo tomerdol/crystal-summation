d9579b70ba9447d695fa4086bf0a0f2e34ef3841
Tomer <tomerdol@post.bgu.ac.il>
2022-07-19 17:39:05 +0300

    implemented summation of the double sum. results agree with python code

--- src/main/java/CrystalSum.java
+++ src/main/java/CrystalSum.java
@@ -14,8 +14,9 @@ public class CrystalSum {
         }
     }
 
-    public static RealVector getLocation(int n, int Lx, int Ly, int Lz, int num_in_cell, RealVector[] latticeVectors, double[][] basis){
-        int k = ((n/num_in_cell)/Lx)/Ly/8%Lz;
+    public static RealVector getLocation(int n, int[] limitingBox, int num_in_cell, RealVector[] latticeVectors, double[][] basis){
+        int Lx = limitingBox[0], Ly = limitingBox[1];
+        int k = ((n/num_in_cell)/Lx)/Ly/8;
         int sym_op = ((n/num_in_cell)/Lx)/Ly%8;
         int j = ((n/num_in_cell)/Lx)%Ly;
         int i = (n/num_in_cell)%Lx;
@@ -26,17 +27,79 @@ public class CrystalSum {
         int[] locCoordinates = new int[]{i, j, k};
         RealVector loc = new ArrayRealVector(3);
         for (int coor=0;coor<latticeVectors.length;coor++){
-            loc=loc.add(locCoordinates[coor], latticeVectors[coor]);	// add primitive lattice vectors according to (i,j,k)
-            loc=loc.add(basis[l][coor],latticeVectors[coor]);	// add fraction of primitive lattice vector according to basis index l
+            loc.combineToSelf(1, locCoordinates[coor], latticeVectors[coor]);   // add primitive lattice vectors according to (i,j,k)
+            loc.combineToSelf(1, basis[l][coor], latticeVectors[coor]);         // add fraction of primitive lattice vector according to basis index l
+            if (coor != 2 && symVec[coor] != 0) {
+                loc.combineToSelf(1, -limitingBox[coor], latticeVectors[coor]);
+            }
         }
-        RealVector position = latticeVectors[0]*i +
+        if (symVec[2]==1){
+            loc = loc.subtract(latticeVectors[2].mapMultiply(2*k+1));
+        }
+        return loc;
     }
-    sym_vec=np.array(())
-            pos = (lattice_vectors[0]*i+lattice_vectors[1]*j+lattice_vectors[2]*k+basis_vectors[l]
-            - sym_vec[0]*lattice_vectors[0]*Lx - sym_vec[1]*lattice_vectors[1]*Ly)
-    if sym_vec[2]==1:
-    pos = pos - (2*k+1)*lattice_vectors[2]
-            return pos
+
+    public static double directSum(RealVector[] latticeVectors, double[][] basis, double heightTol, double radiusTol, int num_in_cell){
+        RealVector origin = new ArrayRealVector(3); // constructs a vector of 3 zeros
+        double prevSum=0, LzSum=0;
+        for (int Lx=1; Lx<300; Lx++){
+            int Ly=Lx;
+            double prevLzSum=0;
+            LzSum=0;
+            int i;
+            for (i=1;; i++) {
+                RealVector ri = getLocation(i, new int[]{Lx, Ly}, num_in_cell, latticeVectors, basis);
+                if (ri.getSubVector(0, 2).getNorm() < Lx) {
+//                    LzSum += dipolar(2, 2, origin, ri);
+                    LzSum += calcPair(origin, ri, 0.00000005, 0.00005, num_in_cell, latticeVectors, basis);
+                }
+                if (i % (num_in_cell * Lx * Ly * 8) == 0 && i / (num_in_cell * Lx * Ly * 8) > Lx) {    // a new "floor" is added at the top and bottom AND the height of the cylinder is larger than its basis radius
+                    System.out.println("Lx\t" + Lx + " Lz\t" + (i / (num_in_cell * Lx * Ly * 8)) + " Lz_sum\t " + LzSum);
+                    if (Math.abs((LzSum - prevLzSum)/LzSum) < heightTol) {  // convergence
+                        System.out.println("Lz convergence! \t Lx\t" + Lx + " Lz\t" + (i / (num_in_cell * Lx * Ly * 8)) + " Lz_sum\t " + LzSum);
+                        break;
+                    }
+                    prevLzSum = LzSum;
+                }
+            }
+            if (Math.abs((LzSum - prevSum)/LzSum) < radiusTol){
+                System.out.println("Lx convergence!! \t Lx\t" + Lx + " Lz\t" + (i / (num_in_cell * Lx * Ly * 8)) + " Lz_sum\t " + LzSum);
+                break;
+            }
+            prevSum=LzSum;
+        }
+
+        return LzSum;
+    }
+
+    public static double calcPair(RealVector ri, RealVector rj, double heightTol, double radiusTol, int num_in_cell, RealVector[] latticeVectors, double[][] basis){
+        double prevSum=0, LzSum;
+        for (int Lx = (int)(ri.subtract(rj).getSubVector(0,2).getNorm()+1);;Lx++){
+            int Ly = Lx;
+            double prevLzSum=0;
+            LzSum=0;
+            int k;
+            for (k=1;;k++){
+                RealVector rk = getLocation(k, new int[]{Lx, Ly}, num_in_cell, latticeVectors, basis);
+                if (rk.getSubVector(0, 2).getNorm() < Lx && !ri.equals(rk) && !rj.equals(rk)){
+                    LzSum += dipolar(0, 2, ri,rk) * dipolar(0, 2, rk,rj);
+                }
+                if (k % (num_in_cell * Lx * Ly * 8) == 0 && k / (num_in_cell * Lx * Ly * 8) > Lx) {    // a new "floor" is added at the top and bottom AND the height of the cylinder is larger than its basis radius
+                    if (Math.abs((LzSum - prevLzSum)/LzSum) < heightTol) {  // convergence
+//                        System.out.println("Lz convergence! \t Lx\t" + Lx + " Lz\t" + (k / (num_in_cell * Lx * Ly * 8)) + " Lz_sum\t " + LzSum);
+                        break;
+                    }
+                    prevLzSum = LzSum;
+                }
+            }
+            if (Math.abs((LzSum - prevSum)/LzSum) < radiusTol){
+//                System.out.println("Lx convergence!! \t Lx\t" + Lx + " Lz\t" + (k / (num_in_cell * Lx * Ly * 8)) + " Lz_sum\t " + LzSum);
+                break;
+            }
+            prevSum=LzSum;
+        }
+        return LzSum;
+    }
 
     public static void main(String[] args){
         double a=1;
@@ -54,6 +117,6 @@ public class CrystalSum {
         };
 
         RealVector origin = new ArrayRealVector(3); // constructs a vector of 3 zeros
-
+        System.out.println(directSum(latticeVectors, basis, 0.005, 0.005, num_in_cell));
     }
 }
